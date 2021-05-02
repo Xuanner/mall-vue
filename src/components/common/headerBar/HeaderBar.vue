9cd776b171f2eff7fc027268f88c919b585bbac7
xuan <13228002836@163.com>
2021-05-02 19:26:59 +0800

    implement the header bar component

---
+++ src/components/common/headerBar/HeaderBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='header-bar'>
+    <div class='left'><slot name='left'></slot></div>
+    <div class='center'><slot name='center'></slot></div>
+    <div class='right'><slot name='right'></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HeaderBar'
+  }
+</script>
+
+<style scoped>
+  .header-bar{
+    display: flex;
+    height: 44px;
+    align-items: center;
+    box-shadow: 0 -1px 10px #DCDCDC;
+    text-align: center;
+  }
+  .left, .right  {
+    width: 60px;
+  }
+  .center {
+    flex: 1
+  }
+</style>
+
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>home</div>
+  <div id='home'>
+    <header-bar>
+      <template v-slot:center>
+        <div>{{pageTitle}}</div>
+      </template>
+    </header-bar>
+  </div>
 </template>
 
 <script>
+  import HeaderBar from 'components/common/headerBar/HeaderBar'
   export default {
-    name: 'Home'
+    name: 'Home',
+    data() {
+      return {
+        pageTitle: 'Shoping'
+      }
+    },
+    components: {
+      HeaderBar
+    }
   }
 </script>
 
 <style scoped>
-
+  
 </style>
 
 
